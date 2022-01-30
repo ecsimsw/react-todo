59653872a11ede34afb12bd6f47b72c15571f860
ecsimsw <ecsimsw@gmail.com>
2022-01-30 17:43:03 +0900

    feat : restart / init

--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const DATA = [
-    { id: "todo-0", name: "Eat", completed: false },
-    { id: "todo-1", name: "Sleep", completed: true },
-    { id: "todo-2", name: "Repeat", completed: false }
-];
-
 ReactDOM.render(
   <React.StrictMode>
-    <App tasks={DATA}/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
